121f26e5083ce251aa996da2de8dc7d34ae3c91a
Discover <81687616+J0CKSTRAP@users.noreply.github.com>
2024-02-26 17:57:45 -0500

    Update rainhubforshitsploits.lua

--- rainhubforshitsploits.lua
+++ rainhubforshitsploits.lua
@@ -1354,7 +1354,47 @@ Instances.CPTitle.Text = "Color Picker"
 Instances.CPTitle.TextSize = 22
 Instances.CPTitle.FontFace = Font.new("rbxassetid://12187365364", Enum.FontWeight.Regular, Enum.FontStyle.Normal)
 
---[[ End of generation. ]]
+
+
+local gui = Instances.Window
+
+local dragging
+local dragInput
+local dragStart
+local startPos
+
+local function update(input)
+	local delta = input.Position - dragStart
+	gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+end
+
+gui.InputBegan:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+		dragging = true
+		dragStart = input.Position
+		startPos = gui.Position
+
+		input.Changed:Connect(function()
+			if input.UserInputState == Enum.UserInputState.End then
+				dragging = false
+			end
+		end)
+	end
+end)
+
+gui.InputChanged:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+		dragInput = input
+	end
+end)
+
+UserInputService.InputChanged:Connect(function(input)
+	if input == dragInput and dragging then
+		update(input)
+	end
+end)
+
+
 
 local Rain = Instances.RainV2
 Rain.Name = "rain"
